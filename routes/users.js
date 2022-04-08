478eb8c32ff1df38626bc78dfcd5e4cf956d6b02
Rahul Hazarika <101569226+RahulHazarika12@users.noreply.github.com>
2022-04-08 17:44:50 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -18,9 +18,13 @@ const users = [
 //all routes here are starting with /users
 router.get("/", (req, res) => {
   console.log(users);
-  res.send("Hello");
+  res.send(users);
 });
 
-router.post("/", (req, res) => {});
+router.post("/", (req, res) => {
+  console.log("POST ROUTE REACHED");
+
+  res.send("POST ROUTE REACHED");
+});
 
 export default router;
